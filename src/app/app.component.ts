5119261d4a7b1dd06e6a4b58094638dde353070a
Guillaume Bedard <guillaume.bedard@planethoster.info>
2020-12-29 16:52:30 -0500

    Transloco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private service: TranslocoService) {}
+
+  ngOnInit() {}
   title = 'transloco';
+
+  change(lang: string) {
+    this.service.setActiveLang(lang);
+  }
 }
